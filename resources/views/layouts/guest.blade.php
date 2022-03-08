42ffb20ea0afb9080c53bae0312cb66f2cde9c43
Isaac Tan <iztan98@gmail.com>
2022-03-08 18:34:05 +0800

    fix: remove midone js testing

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/midone.js') }}" defer></script>
+        {{-- <script src="{{ asset('js/midone.js') }}" defer></script> --}}
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
